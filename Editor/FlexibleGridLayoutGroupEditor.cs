c741a865996d12e797c0d9bcb69e9a769b7c9df0
ADaurio <arthur.daurio@gmail.com>
2019-09-04 02:05:35 -0400

    Initial push

---
+++ Editor/FlexibleGridLayoutGroupEditor.cs
@@ -0,0 +1,74 @@
+﻿#if UNITY_EDITOR
+using UnityEngine.UI;
+
+namespace UnityEditor.UI
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(FlexibleGridLayoutGroup), true)]
+    public class FlexibleGridLayoutGroupEditor : Editor
+    {
+        SerializedProperty m_Padding;
+        SerializedProperty m_CellSize;
+        SerializedProperty m_Spacing;
+        SerializedProperty m_StartCorner;
+        SerializedProperty m_StartAxis;
+        SerializedProperty m_ChildAlignment;
+        SerializedProperty m_Constraint;
+        SerializedProperty m_ConstraintCount;
+        SerializedProperty m_OffAxisScaling;
+        SerializedProperty m_OffAxisCount;
+        SerializedProperty m_OffAxisAspectRatio;
+
+        protected virtual void OnEnable()
+        {
+            m_Padding = serializedObject.FindProperty("m_Padding");
+            m_CellSize = serializedObject.FindProperty("m_CellSize");
+            m_Spacing = serializedObject.FindProperty("m_Spacing");
+            m_StartCorner = serializedObject.FindProperty("m_StartCorner");
+            m_StartAxis = serializedObject.FindProperty("m_StartAxis");
+            m_ChildAlignment = serializedObject.FindProperty("m_ChildAlignment");
+            m_Constraint = serializedObject.FindProperty("m_Constraint");
+            m_ConstraintCount = serializedObject.FindProperty("m_ConstraintCount");
+            m_OffAxisScaling = serializedObject.FindProperty("m_OffAxisScaling");
+            m_OffAxisCount = serializedObject.FindProperty("m_OffAxisCount");
+            m_OffAxisAspectRatio = serializedObject.FindProperty("m_OffAxisAspectRatio");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(m_Padding, true);
+            EditorGUILayout.PropertyField(m_StartCorner, true);
+            EditorGUILayout.PropertyField(m_StartAxis, true);
+            EditorGUILayout.PropertyField(m_ChildAlignment, true);
+            EditorGUILayout.PropertyField(m_Constraint, true);
+            EditorGUI.indentLevel++;
+
+            if (m_Constraint.enumValueIndex > 0)
+            {
+                EditorGUILayout.PropertyField(m_ConstraintCount, true);
+                EditorGUILayout.PropertyField(m_OffAxisScaling, true);
+
+                if (m_OffAxisScaling.enumValueIndex == 0)
+                {
+                    EditorGUILayout.PropertyField(m_OffAxisCount, true);
+                }
+                else
+                {
+                    EditorGUILayout.PropertyField(m_OffAxisAspectRatio, true);
+                }
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(m_CellSize, true);
+                EditorGUILayout.PropertyField(m_Spacing, true);
+            }
+
+            EditorGUI.indentLevel--;
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+#endif
---
+++ FlexibleGridLayoutGroup.cs
@@ -0,0 +1,81 @@
+﻿namespace UnityEngine.UI
+{
+    [AddComponentMenu("Layout/Flexible Grid Layout Group", 152)]
+    public class FlexibleGridLayoutGroup : GridLayoutGroup
+    {
+        public enum OffAxisCellSize
+        {
+            Flexible = 0,
+            Count,
+            AspectRatio
+        }
+
+        [SerializeField]
+        protected OffAxisCellSize m_OffAxisScaling = OffAxisCellSize.Flexible;
+        public OffAxisCellSize offAxisScaling { get { return m_OffAxisScaling; } set { SetProperty(ref m_OffAxisScaling, value); } }
+
+        [SerializeField]
+        protected int m_OffAxisCount = 1;
+        public int offAxisCount { get { return m_OffAxisCount; } set { SetProperty(ref m_OffAxisCount, Mathf.Max(0, value)); } }
+
+        [SerializeField]
+        protected float m_OffAxisAspectRatio = 1;
+        public float offAxisAspectRatio { get { return m_OffAxisAspectRatio; } set { SetProperty(ref m_OffAxisAspectRatio, Mathf.Max(0, value)); } }
+        
+        public override void SetLayoutHorizontal()
+        {
+            UpdateCellSize();
+            base.SetLayoutHorizontal();
+        }
+
+        public override void SetLayoutVertical()
+        {
+            UpdateCellSize();
+            base.SetLayoutVertical();
+        }
+
+        protected void UpdateCellSize()
+        {
+            if (constraint == Constraint.FixedColumnCount)
+            {
+                RectTransform rT = GetComponent<RectTransform>();
+                Vector2 newCellSize = new Vector2((rT.rect.width - padding.horizontal) / constraintCount, 0);
+
+                if (offAxisScaling == OffAxisCellSize.Flexible)
+                {
+                    newCellSize.y = (rT.rect.height - padding.vertical) / rectChildren.Count;
+                }
+                else if (offAxisScaling == OffAxisCellSize.Count)
+                {
+                    newCellSize.y = (rT.rect.height - padding.vertical) / offAxisCount;
+                }
+                else
+                {
+                    newCellSize.y = newCellSize.x * offAxisAspectRatio;
+                }
+
+                cellSize = newCellSize;
+            }
+            else if (constraint == Constraint.FixedRowCount)
+            {
+                RectTransform rT = GetComponent<RectTransform>();
+                Vector2 newCellSize = new Vector2(0, (rT.rect.height - padding.vertical) / constraintCount);
+
+                if (offAxisScaling == OffAxisCellSize.Flexible)
+                {
+                    newCellSize.x = (rT.rect.width - padding.horizontal) / rectChildren.Count;
+                }
+                else if (offAxisScaling == OffAxisCellSize.Count)
+                {
+                    newCellSize.x = (rT.rect.width - padding.horizontal) / offAxisCount;
+                }
+                else
+                {
+                    newCellSize.x = newCellSize.y * offAxisAspectRatio;
+                }
+
+                cellSize = newCellSize;
+            }
+        }
+    }
+}
